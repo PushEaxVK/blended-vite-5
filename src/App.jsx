3ad118dc0c9022887e95257f531f77189a17aca6
PushEax <pusheax.vk@gmail.com>
2025-05-18 14:54:34 +0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { lazy, Suspense } from 'react';
 import Heading from './components/Heading/Heading';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header.jsx';
+
+const Home = lazy(() => import('./pages/Home.jsx'));
+const Rates = lazy(() => import('./pages/Rates.jsx'));
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="rates" element={<Rates />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
